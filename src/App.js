ee22e0ab9fe85b3438f2ffcf43fa435c1a9536d3
Anna Koval <106483373+AnnyKoval@users.noreply.github.com>
2022-08-07 10:15:24 +0300

    created a new weather component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Kyiv" />
+        <footer>
+          This project is coded by{" "}
+          <a href="https://annakoval-cv.netlify.app/"> Anna Koval</a> and
+          open-sourced on{" "}
+          <a
+            href="https://github.com/AnnyKoval/weather-final-react"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
